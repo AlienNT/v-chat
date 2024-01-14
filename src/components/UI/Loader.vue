1bcb9eb0a05986f9a459be4662d8ce793069b4b8
Trysten M <nick.tristen22@gmail.com>
2024-01-14 13:17:07 +0200

    add(components): add loader

---
+++ src/components/UI/Loader.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+import Read from "@/components/UI/icons/Read.vue";
+</script>
+
+<template>
+  <div class="loader-wrapper">
+    <Read class="loader-item"/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.loader-item {
+  animation: 1s infinite linear rotated;
+}
+
+@keyframes rotated {
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
