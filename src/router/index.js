b9478641b541a9cbbc513eb3dccf5034272a7793
Trysten M <nick.tristen22@gmail.com>
2024-01-15 21:24:51 +0200

    update(router): add child routes to auth

--- src/router/index.js
+++ src/router/index.js
@@ -9,12 +9,36 @@ const router = createRouter({
         {
             path: routerNames.AUTH_PAGE.path,
             name: routerNames.AUTH_PAGE.name,
-            component: () => import('@/pages/AuthPage.vue')
+            component: () => import('@/pages/AuthPage.vue'),
+            children: [
+                {
+                    path: '',
+                    name: 'login',
+                    component: () => import('@/components/auth/LoginForm.vue')
+                },
+                {
+                    path: '',
+                    name: 'registration',
+                    component: () => import('@/components/auth/RegistrationForm.vue')
+                },
+            ]
         },
         {
             path: routerNames.CHATS_PAGE.path,
             name: routerNames.CHATS_PAGE.name,
-            component: () => import('@/pages/ChatsPage.vue')
+            component: () => import('@/pages/ChatsPage.vue'),
+            children: [
+                {
+                    path: ':id',
+                    name: 'chat-window',
+                    component: () => import('@/components/chatWindow/ChatWindow.vue')
+                },
+                {
+                    path: '',
+                    name: 'empty-title',
+                    component: () => import('@/components/chatWindow/ChatWindowEmptyTitle.vue')
+                }
+            ]
         },
         {
             path: routerNames.SETTINGS_PAGE.path,
