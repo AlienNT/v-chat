56eba4c65bf7e7fa89f11b34cb2725dc524f22ba
Trysten M <nick.tristen22@gmail.com>
2024-01-19 02:50:46 +0200

    update(settings): add logout button

--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -1,11 +1,34 @@
 <script setup>
+import VButton from "@/components/UI/VButton.vue";
+import {useAuth} from "@/composables/store/useAuth.js";
+import {useProfile} from "@/composables/store/useProfile.js";
+import {useMessages} from "@/composables/store/useMessages.js";
+import {useDialogs} from "@/composables/store/useDialogs.js";
 
+const {resetToken} = useAuth()
+const {resetProfile} = useProfile()
+const {resetMessages} = useMessages()
+const {resetDialogs} = useDialogs()
+
+function logout() {
+  resetToken()
+  resetProfile()
+  resetMessages()
+  resetDialogs()
+}
 </script>
 
 <template>
-
+  <div class="button-wrapper">
+    <VButton
+        title="logout"
+        @click.prevent="logout"
+    />
+  </div>
 </template>
 
 <style scoped>
-
+.button-wrapper {
+  margin: auto;
+}
 </style>
