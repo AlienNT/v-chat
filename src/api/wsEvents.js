77d239b96fd29f0141c148f936d168fcd99b4afe
Trysten M <nick.tristen22@gmail.com>
2024-01-19 02:32:16 +0200

    new(api): add wsEvents.js

---
+++ src/api/wsEvents.js
@@ -0,0 +1,19 @@
+export const wsEvents = {
+    LOGIN: 'login',
+    LOGOUT: 'logout',
+    REGISTRATION: 'registration',
+    CREATE_DIALOG: 'createDialog',
+    UPDATE_DIALOG: 'updateDialog',
+    GET_DIALOGS: 'getDialogs',
+    DELETE_DIALOG: 'deleteDialog',
+    ADD_MESSAGE: 'addMessage',
+    UPDATE_MESSAGE: 'updateMessage',
+    DELETE_MESSAGE: 'deleteMessage',
+    GET_MESSAGES: 'getMessages',
+    RESPONSE_STATUS: 'responseStatus',
+    GET_PROFILE: 'getProfile',
+    UPDATE_PROFILE: 'updateProfile',
+    GET_SETTINGS: 'getSettings',
+    UPDATE_SETTINGS: 'updateSettings'
+
+}
