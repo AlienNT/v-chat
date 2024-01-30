04f97fd65f91b07c3c5bd06704272955b35763aa
Trysten M <nick.tristen22@gmail.com>
2024-01-30 23:52:42 +0200

    add(helper): add get style method

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -26,3 +26,11 @@ export function toBase64(file) {
 export function objectFieldsToString({object = {}, separator = ' ', fields = []}) {
     return fields.length ? fields.map(field => object?.[field] || '').join(separator) : ''
 }
+
+export function getStyles({backgroundColor, maskImage, flex}) {
+    return [
+        backgroundColor ? `background-color: ${backgroundColor}` : '',
+        maskImage ? `mask-image: url(${maskImage})` : '',
+        flex ? `flex ${flex}%` : '',
+    ].join('; ')
+}
