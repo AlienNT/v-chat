5de119e4b1837abcc20608f8dc7111da8f9151a5
Trysten M <nick.tristen22@gmail.com>
2024-01-31 01:38:07 +0200

    update(components/chatWindow): add strolling to last message

--- src/components/chatWindow/ChatWindowMessages.vue
+++ src/components/chatWindow/ChatWindowMessages.vue
@@ -1,5 +1,7 @@
 <script setup>
 import ChatWindowMessage from "@/components/chatWindow/ChatWindowMessage.vue";
+import {onMounted, reactive, ref, watch} from "vue";
+import {_} from "@/helpers/fakeLodash.js";
 
 const props = defineProps({
   messages: {
@@ -9,26 +11,59 @@ const props = defineProps({
     type: String
   }
 })
+const state = reactive({
+  debounce: null
+})
+
+const chatMessagesViewport = ref(null)
+
+function scrollToLastMessage(target) {
+  return target.parentNode.scrollBy({
+    top: target.scrollHeight,
+    behavior: 'auto'
+  })
+}
+
+onMounted(() => {
+  state.debounce = _.debounce(() => scrollToLastMessage(chatMessagesViewport.value))
+})
+
+watch(() => props.messages?.length, (value) => {
+  value && chatMessagesViewport.value && state.debounce()
+}, {
+  immediate: true
+})
 
 </script>
 
 <template>
-  <ul class="chat-messages scroll">
-    <ChatWindowMessage
-        v-for="message in messages"
-        class="chat-message"
-        :message="message"
-        :user-id="userId"
-    />
-  </ul>
+  <div class="chat-messages__wrapper scroll">
+    <ul
+        class="chat-messages"
+        ref="chatMessagesViewport"
+    >
+      <ChatWindowMessage
+          v-for="message in messages"
+          class="chat-message"
+          :message="message"
+          :user-id="userId"
+      />
+    </ul>
+  </div>
 </template>
 
 <style scoped lang="scss">
 @import "../../assets/css/scrollbar";
 
-.chat-messages {
+.chat-messages__wrapper {
   flex: 1;
-  overflow-y: auto;
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.chat-messages {
+  flex: 100%;
   display: flex;
   flex-direction: column;
   gap: 1rem;
