1445c73c7d0abc5bc13fd4ecbe324574a9bc6f05
Trysten M <nick.tristen22@gmail.com>
2024-01-31 23:36:01 +0200

    update(store) switch messages store js to ts

--- src/composables/store/useMessages.ts
+++ src/composables/store/useMessages.ts
@@ -1,25 +1,29 @@
 import {computed, reactive} from "vue";
 
-const state = reactive({
+import {Message, MessagesState, SetMessage, SetMessages} from "../../interfaces/message";
+import {Id} from "../../interfaces";
+
+const state: MessagesState = reactive({
     dialogs: {},
     members: []
 })
 
 export function useMessages() {
-    const messages = (dialogId) => computed(() => {
+    const messages = (dialogId: Id) => computed((): Message[] => {
         return state.dialogs[dialogId]
     })
 
-    function setMessages({dialogId, messages}) {
+    function setMessages({dialogId, messages}: SetMessages): void {
         state.dialogs[dialogId] = messages
     }
 
-    function setMessage({dialogId, message}) {
+    function setMessage({dialogId, message}: SetMessage): void {
         !state.dialogs[dialogId] ?
             state.dialogs[dialogId] = [message] :
             state.dialogs[dialogId].push(message)
     }
-    function resetMessages() {
+
+    function resetMessages(): void {
         state.dialogs = {}
         state.members = []
     }
