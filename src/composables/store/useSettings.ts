b869d39f64092ccf7c655761fd22d1899a753f33
Trysten M <nick.tristen22@gmail.com>
2024-01-31 23:35:42 +0200

    update(store) switch settings store js to ts

---
+++ src/composables/store/useSettings.ts
@@ -0,0 +1,27 @@
+import {computed, reactive} from "vue";
+
+import {Settings, SettingsState} from "../../interfaces/settings";
+
+const state: SettingsState = reactive({
+    settings: null
+})
+
+export function useSettings(): object {
+    const settings = computed((): Settings => {
+        return state.settings
+    })
+
+    function setSettings(data: Settings): void {
+        state.settings = data
+    }
+
+    function resetSettings() {
+        state.settings = null
+    }
+
+    return {
+        settings,
+        setSettings,
+        resetSettings
+    }
+}
